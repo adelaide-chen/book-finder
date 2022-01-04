d533997271ab21b77ca4f8e93a4185021274cddf
Ada Chen <ada.chen@berkeley.edu>
2022-01-03 23:27:48 -0800

    some ui stuff but i have to finish the backend/db

--- react-gh-pages/src/index.js
+++ react-gh-pages/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StyledEngineProvider injectFirst>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StyledEngineProvider>,
+  document.querySelector("#root")
 );
 
 // If you want to start measuring performance in your app, pass a function
