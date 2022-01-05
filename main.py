36acfb525f5fb3b96ad93c217dfb2286e1cb70e6
Ada Chen <ada.chen@berkeley.edu>
2022-01-05 00:44:07 -0800

    database working, moved credentials to gitignore files

--- main.py
+++ main.py
@@ -1,51 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
 import pyrebase
+import api_things
 
+# config for firebase's API
+FIREBASE = pyrebase.initialize_app(api_things.CONFIG)
+DATABASE = FIREBASE.database()
 
-CONFIG = {
-  "apiKey": "AIzaSyCppLTkbikfSJBXS5aY9uW7iEoqQsEHlz4",
-  "authDomain": "find-books-by-rating.firebaseapp.com",
-  "databaseURL": "https://find-books-by-rating-default-rtdb.firebaseio.com",
-  "projectId": "find-books-by-rating",
-  "storageBucket": "find-books-by-rating.appspot.com",
-  "messagingSenderId": "590127508190",
-  "appId": "1:590127508190:web:8884527a31e6bfac93b04f",
-  "measurementId": "G-DEYZRLW2V0"
-}
-
-FIREBASE = pyrebase.initialize_app(CONFIG)
-
-API_KEY = "1OAz00GvVLfXiOnfQ6BP1M2GVRS8qpCa"
-
-response = requests.get(f"https://api.nytimes.com/svc/books/v3/lists/names?api-key={API_KEY}")
-body = response.json()
+# API key for NY Times
+API_KEY = api_things.API_KEY
 
 
 def cleanup_string(text):
     # result from HTML tags extracted with BeautifulSoup
-    return [string.strip() for string in text.split('\n') if string is not ''][0]
+    return [string.strip() for string in text.split("\n") if string != ""][0]
 
 
 def get_goodreads_info(isbn):
     # Given ISBN, return rating value and count for book from Goodreads
     response = requests.get(f"https://www.goodreads.com/search?q={isbn}")
-    soup = BeautifulSoup(response.text, 'html.parser')
-    rating_value = cleanup_string(soup.find(itemprop='ratingValue').text)
-    rating_count = cleanup_string(soup.find(itemprop='ratingCount').text)
-    title = soup.find(attrs={'property': 'og:title'})['content']
-    author = soup.find(attrs={'class': 'authorName'}).find(itemprop='name').text
-    link = soup.find(attrs={'property': 'og:url'})['content']
-    return (title, author, rating_value, rating_count, link)
+    soup = BeautifulSoup(response.text, "html.parser")
+    rating_value = cleanup_string(soup.find(itemprop="ratingValue").text)
+    rating_count = cleanup_string(soup.find(itemprop="ratingCount").text)
+    title = soup.find(attrs={"property": "og:title"})["content"]
+    author = soup.find(attrs={"class": "authorName"}).find(itemprop="name").text
+    link = soup.find(attrs={"property": "og:url"})["content"]
+    print(title)
+    return {
+        "title": title,
+        "author": author,
+        "rating_value": rating_value,
+        "rating_count": rating_count,
+        "link": link,
+        "isbn": isbn,
+    }
 
 
-def get_books(genres, date=None):
-    # Given genre from NYTimes bestsellers list, return Goodreads rating values and counts for each book
-    books = []
-    for genre in genres:
-        response = requests.get(f"https://api.nytimes.com/svc/books/v3/lists.json?list={genre}&api-key={API_KEY}")
-        result = response.json()['results']
-        books += [get_goodreads_info(book['book_details'][0]['primary_isbn13']) for book in result]
-    return books
+def add_books_by_genre_and_date(genre, date=None):
+    # Given genre and date from NYTimes bestsellers list, retrieve Goodreads details for each book and push to database
+    response = requests.get(
+        f"https://api.nytimes.com/svc/books/v3/lists.json?list={genre}&api-key={API_KEY}"
+    )
+    result = response.json()["results"]
+    for book in result:
+        data = get_goodreads_info(book["book_details"][0]["primary_isbn13"])
+        push_book_to_db(genre, data)
+    print("done!")
 
-get_books(["espionage"])
+
+def push_book_to_db(genre, book):
+    # Given book details and genre, add to FireBase
+    DATABASE.child(genre).child(book["isbn"]).set(book)
+
+
+# add_books_by_genre_and_date("espionage")
